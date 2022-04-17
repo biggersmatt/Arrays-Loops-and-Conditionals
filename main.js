5e424fe820dc691734f36f0de4f1c8a74883b1e8
biggersmatt <biggersmatt73@gmail.com>
2022-04-16 22:01:32 -0700

    Completed all

--- main.js
+++ main.js
@@ -40,14 +40,44 @@ const thirdProblem = "fizzBuzz";
 
 for (let i = 1; i <= 100; i++) {
   if ((i % 3 === 0) && (i % 5 === 0)) {
-    console.log(thirdProblem);
+    //console.log(thirdProblem);
   } else if (i % 3 === 0) {
-    console.log(firstProblem);
+    //console.log(firstProblem);
   } else if (i % 5 === 0) {
-    console.log(secondProblem)
+    //console.log(secondProblem)
   } else {
-    console.log(i);
+    //console.log(i);
   }
+}
+
+// Getting to Know You
+// Use the following arrays to answer the questions below (name, age, hometown):
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "gameboy";
+//console.log(kenny)
+//console.log(jimClark[1] + 1)
+ryan[2] = "Gotham City";
+//console.log(ryan[2])
+reuben.pop();
+reuben.push("Chicago");
+//console.log(reuben)
+jimHaff.pop()
+jimHaff.push("San Francisco", "Atlanta", "Oakland");
+//console.log(jimHaff)
+
+// Yell at the Ninja Turtles
+// Create an array with the members of the ninja turtles 
+//(Donatello, Leonardo, Raphael, Michaelangelo)
+
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+for (let i = 0; i < ninjaTurtles.length; i++) {
+  //console.log(ninjaTurtles[i].toUpperCase())
 }
 
 
@@ -56,14 +86,63 @@ for (let i = 1; i <= 100; i++) {
 
 
 
+// Return of the Closets
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+const kristynsShoe = kristynsCloset.shift();
+thomsCloset[2].push(kristynsShoe);
+// console.log(thomsCloset[2])
+
+const outfit = ('Today Thom is wearing a ' + thomsCloset[0][1] + ' and ' + thomsCloset[1][0] + ' with ' + thomsCloset[2][1]);
+
+// console.log(outfit)
+
+for (let i = 0; i < kristynsCloset.length; i++) {
+  //console.log('WHIRR: Now washing ' + kristynsCloset[i])
+}
+for (let i = 0; i < thomsCloset.length; i++) {
+  // console.log(thomsCloset[i])
+}
 
 
 
+let sum = 0;
 
+for (let i = 0; i < 1000; i++) {
+  if(i % 3 == 0 && i % 5 == 0) {
+    sum = sum + i;
+  }
+}
 
-
-
-
+console.log(sum)
 
 
 
